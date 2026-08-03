b70fa241b348a7932d06eb94062d1e394b248f53
Saurabh Pandey <shaurabhipandey@gmail.com>
2026-08-03 12:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (68.10%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -9,30 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-int cameras=0;
-int min_camera(TreeNode* root){
-    if(root==NULL){
-        return 1;
-    }
-    int lc=min_camera(root->left);
-    int rc=min_camera(root->right);
 
-    if(lc==-1 || rc==-1){
-        cameras++;
-        return 0;
-    }
-    if(lc==0||rc==0){
-        return 1;
-    }
-    return -1;
-}
-    int minCameraCover(TreeNode* root) {
-        if(min_camera(root)==-1){
+    // State Mapping
+    // -1 = NEEDS_CAMERA
+    //  0 = HAS_CAMERA
+    //  1 = COVERED
+
+    int cameras = 0;
+
+    int dfs(TreeNode* root)
+    {
+        // NULL node ko already covered maanenge.
+        if(root == NULL)
+            return 1;
+
+        // Bottom-Up DFS
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        // Agar kisi bhi child ko camera chahiye,
+        // to current node pe camera lagana padega.
+        if(left == -1 || right == -1)
+        {
             cameras++;
-            
+            return 0;          // HAS_CAMERA
         }
+
+        // Agar kisi child ke paas camera hai,
+        // to current node automatically covered hai.
+        if(left == 0 || right == 0)
+        {
+            return 1;          // COVERED
+        }
+
+        // Agar dono children covered hain
+        // aur kisi ke paas camera nahi hai,
+        // to current node ko camera chahiye.
+        return -1;             // NEEDS_CAMERA
+    }
+
+    int minCameraCover(TreeNode* root)
+    {
+        // Agar root bhi camera maang raha hai,
+        // to uske liye extra camera lagana padega.
+        if(dfs(root) == -1)
+            cameras++;
+
         return cameras;
     }
 };
