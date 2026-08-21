fac5f3bb1dbc97a8478660495ee53384b43b46ae
Saurabh Pandey <shaurabhipandey@gmail.com>
2026-08-21 23:28:48 +0530

    Time: 3 ms (37.26%) | Memory: 40.1 MB (95.99%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+       vector<int>ans(nums.size(),0);
+       int leftproduct=1;
+       for(int i=0;i<nums.size();i++){
+    
+        ans[i]=leftproduct;
+                leftproduct*=nums[i];
+
+       }
+
+       int rightproduct=1;
+       for(int i=nums.size()-1;i>=0;i--){
+ans[i] *= rightproduct;
+rightproduct *= nums[i];
+       }
+
+       return ans; 
+    }
+};
