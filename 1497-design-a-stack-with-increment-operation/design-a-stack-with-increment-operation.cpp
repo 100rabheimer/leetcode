9f7b623ca1b11a63b36e96b5e9a91c8d3b69c787
Saurabh Pandey <shaurabhipandey@gmail.com>
2026-09-06 20:47:48 +0530

    Time: 6 ms (39.56%) | Memory: 28.7 MB (95.56%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -1,21 +1,77 @@
+// class CustomStack {
+// public:
+//     vector<int> st;
+//     vector<int> inc;
+// int maxSize;
+//     CustomStack(int maxSize) {
+//           this->maxSize = maxSize;
+//   // maxSize ki zarurat is approach mein directly nahi hai
+//     }
+
+//     void push(int x) {
+//         if(st.size() == maxSize)
+//     return;
+//         // normal stack mein push
+//         st.push_back(x);
+
+//         // is element ke liye initially koi increment nahi
+//         inc.push_back(0);
+//     }
+
+//     int pop() {
+//         if (st.empty()) {
+//             return -1;
+//         }
+
+//         int i = st.size() - 1;
+
+//         // is element ka pending increment
+//         int add = inc[i];
+
+//         // actual answer
+//         int answer = st[i] + add;
+
+//         // agar neeche element exist karta hai,
+//         // current ka increment usko pass karo
+//         if (i > 0) {
+//             inc[i - 1] += add;
+//         }
+
+//         // top element remove
+//         st.pop_back();
+//         inc.pop_back();
+
+//         return answer;
+//     }
+
+//     void increment(int k, int val) {
+//         if (st.empty()) {
+//             return;
+//         }
+
+//         // bottom k elements mein se
+//         // sabse upar wale affected element ka index
+//         int i = min(k, (int)st.size()) - 1;
+
+//         // bas marker lagao
+//         inc[i] += val;
+//     }
+// };
+
+
 class CustomStack {
 public:
     vector<int> st;
-    vector<int> inc;
-int maxSize;
+    int maxSize;
+
     CustomStack(int maxSize) {
-          this->maxSize = maxSize;
-  // maxSize ki zarurat is approach mein directly nahi hai
+        this->maxSize = maxSize;
     }
 
     void push(int x) {
-        if(st.size() == maxSize)
-    return;
-        // normal stack mein push
-        st.push_back(x);
-
-        // is element ke liye initially koi increment nahi
-        inc.push_back(0);
+        if (st.size() < maxSize) {
+            st.push_back(x);
+        }
     }
 
     int pop() {
@@ -23,37 +79,16 @@ int maxSize;
             return -1;
         }
 
-        int i = st.size() - 1;
-
-        // is element ka pending increment
-        int add = inc[i];
-
-        // actual answer
-        int answer = st[i] + add;
-
-        // agar neeche element exist karta hai,
-        // current ka increment usko pass karo
-        if (i > 0) {
-            inc[i - 1] += add;
-        }
-
-        // top element remove
+        int top = st.back();
         st.pop_back();
-        inc.pop_back();
-
-        return answer;
+        return top;
     }
 
     void increment(int k, int val) {
-        if (st.empty()) {
-            return;
+        int limit = min(k, (int)st.size());
+
+        for (int i = 0; i < limit; i++) {
+            st[i] += val;
         }
-
-        // bottom k elements mein se
-        // sabse upar wale affected element ka index
-        int i = min(k, (int)st.size()) - 1;
-
-        // bas marker lagao
-        inc[i] += val;
     }
 };
